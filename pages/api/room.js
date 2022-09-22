88570c7180fe86edb9cfef164e520c9d18f07842
gusntc <Natacha_rungbanpant@cmu.ac.th>
2022-09-22 13:57:44 +0700

    api/me

--- pages/api/room.js
+++ pages/api/room.js
@@ -2,4 +2,10 @@ import { readDB } from "../../backendLibs/dbLib";
 
 export default function roomRoute(req, res) {
   const rooms = readDB();
+  const result = [];
+  for (const room of rooms) {
+    result.push({ roomId: room.roomId, roomName: room.roomName });
+  }
+
+  return res.json({ ok: true, rooms: result });
 }
