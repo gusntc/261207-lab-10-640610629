33e54536d226c9d39897ca1a8bdc3bc3e95bfff0
gusntc <Natacha_rungbanpant@cmu.ac.th>
2022-09-22 16:18:42 +0700

    5

--- pages/api/room/[roomId]/message/[messageId].js
+++ pages/api/room/[roomId]/message/[messageId].js
@@ -6,19 +6,31 @@ export default function roomIdMessageIdRoute(req, res) {
 
   const roomId = req.query.roomId;
   const messageId = req.query.messageId;
-
-  const RoomIdx = rooms.findIndex((x) => x.roomId === roomId);
-  const MessageIdx = rooms.findIndex((x) => x.messages.messageId === messageId);
-
+  //----------------------------------------------------------------
   if (req.method === "DELETE") {
-    if (RoomIdx === -1) {
-      return res.status(404).json({ ok: false, message: "Invalid room id" });
-    } else if (MessageIdx === -1) {
-      return res.status(404).json({ ok: false, message: "Invalid message id" });
+    const rooms = readDB();
+    const room = rooms.find((room) => room.roomId === roomId);
+    if (room) {
+      const messageIdx = room.messages.findIndex(
+        (message) => message.messageId === messageId
+      );
+      if (messageIdx === -1) {
+        res.status(404).json({
+          ok: false,
+          message: "Invalid message ID",
+        });
+      } else {
+        room.messages.splice(messageIdx, 1);
+        writeDB(rooms);
+        res.status(200).json({
+          ok: true,
+        });
+      }
     } else {
-      rooms.splice(MessageIdx, 1);
-      writeDB(rooms);
-      return res.status(200).json({ ok: true });
+      res.status(404).json({
+        ok: false,
+        message: "Invalid room ID",
+      });
     }
   }
 }
